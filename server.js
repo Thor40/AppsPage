99413151ccb59bf410a65aec1fa0090fb58bd78d
Thor40 <nicholas.kosik@gmail.com>
2021-01-07 20:45:38 -0600

    connection

--- server.js
+++ server.js
@@ -14,12 +14,17 @@ const server = new ApolloServer({
     context: authMiddleware
   });
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-});
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-});
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      // log where we can go to test our GQL API
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,11 @@
+import { gql, useQuery } from '@apollo/client';
 import { ApolloClient } from '@ApolloClient';
 require('dotenv').config();
 
-const InMemoryCache = require('apollo-boost').InMemoryCache;
 const ApolloClient = require('apollo-boost').ApolloClient;
 
 const HttpLink = require('apollo-boost').HttpLink;
 const appList = new ApolloClient({
-    cache: new InMemoryCache(),
     link: new HttpLink({
         uri: gqluri,
         fetch: fetch
